f99d8e70841648f12b0e73f667051bd8aa1408d9
kevin <xuxieliang@yicai.com>
2025-08-29 14:55:03 +0800

    Initial Commit

---
+++ XLiPadBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XLiPadBook
+//
+//  Created by ice on 29/8/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
